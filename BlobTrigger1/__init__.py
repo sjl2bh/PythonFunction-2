0aea501bbf616b377ae047f3b51dd23053d0660b
Samuel Longo <d.longo@live.com>
2022-11-22 23:11:36 -0500

    .vscode

--- BlobTrigger1/__init__.py
+++ BlobTrigger1/__init__.py
@@ -35,26 +35,10 @@ def main(myblob: func.InputStream):
 
     #download from blob
     blob_service_client_instance = BlobServiceClient(account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
-    blob_client_instance = blob_service_client_instance.get_blob_client(CONTAINERNAME, BLOBNAME, snapshot=None)
-    # with open(LOCALFILENAME, "wb") as CurrentBlob:
-    #     blob_data = blob_client_instance.download_blob()
-    #     blob_data.readinto(CurrentBlob)
-    #     print(blob_data)
-    #     print("THIS IS blob_data TYPE: " + str(type(CurrentBlob)))
-        
     container_client = blob_service_client_instance.get_container_client(container= "nlpinputs") 
-    
-    st = container_client.download_blob(BLOBNAME).download_to_stream()
-    download_file = open(file="BlobTrigger1\\Data_Holder_Folder\\FileyTheFile.xlsx", mode="wb+") 
-    download_file.write(container_client.download_blob(BLOBNAME).readall())
-    download_file.close()
-    
-    # dataframe_blobdata = pd.ExcelFile("BlobTrigger1\\Data_Holder_Folder\\FileyTheFile.xlsx")
-    # file_name = open(LOCALFILENAME,'r', encoding='utf-8')
-    # print(type(file_name))
-    # print(file_name)
-    # xl_workbook = pd.ExcelFile(file_name)
-    # input_df = xl_workbook.parse("DataSheet")  # Parse the sheet into a dataframe
-    # print(input_df)    
-    # #t2=time.time()
-    # #print(("It takes %s seconds to download "+BLOBNAME) % (t2 - t1))
+    blob_client = container_client.get_blob_client(BLOBNAME)
+
+    with open(file="BlobTrigger1\\Data_Holder_Folder\\FileyTheFile.xlsx", mode="wb") as blob:
+        stream = blob_client.download_blob()
+        blob.write(stream.readall())
+        blob.close()
